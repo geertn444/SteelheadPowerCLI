e9c7bb03d76d5b7a79d4908384f6d4cf28c29c76
geertn444 <geert.nijs@gmail.com>
2015-03-11 13:41:28 +0100

    pandas conversion
    -removed show opt_ssl , use show opt tcp 443 which is just the same
    -reworked statistics to pandas module

--- rvbd.py
+++ rvbd.py
@@ -10,6 +10,8 @@ import sys
 import argparse
 import re
 import json
+import pandas as pd
+import numpy as np
 
 class RunCommand(cmd.Cmd):
  """ Simple shell to run a command on the host """
@@ -118,25 +120,20 @@ set pwd <pwd>           : Default password"""
    else: print ('Please provide password. set pwd <password>')
  
 
- def do_remove(self, args):
-  """remove host <ip>: Remove a host with ip <ip> from the host list [NOT IMPLEMENTED YET]."""
-  l = args.split()
-  if len(l) < 1:
-   print "Add needs an argument. Do 'help remove'"
-   return
-  if l[0] <> 'host':
-   print "Unknown argument. Do 'help remove'"
-   return
-  log.info( "TO BE IMPLEMENTED")
- 
+ def do_remove_host(self, args):
+  """remove_host : Remove a host from the host list [NOT IMPLEMENTED YET]."""
+  if args:
+   log.info( "to be implemented")
+  else:
+   print "Usage: remove_host <ip> "
   
  def do_show(self, args):
   """show hosts     : View configured hosts
 show opt       : Show summary of optimized connections
-show opt_ssl   : Show summary of optimized SSL connections
 show pass      : Show summary of passthrough connections
 show preex     : Show summary of preexisting sessions
 show set       : Show settings"""
+
   l = args.split()
   if len(l) < 1:
    print "Show needs an argument. Do 'help show'"
@@ -230,10 +227,14 @@ show set       : Show settings"""
 
      count = 0
      rij = []
+     pandas_rij = []
+     rijdict = {}
+     
      for line in StringIO.StringIO(opt_conn):
       line = line.replace("\n","")
       if line.startswith(cli_command[2]):
        parts = line.split()
+       rijdict = {}
        if (self.debug): log.debug("Parts")
        if (self.debug): log.debug(parts)
        source = parts[1]
@@ -246,34 +247,63 @@ show set       : Show settings"""
        #datum = parts[5]
        #tijd = parts[6]
        #print sourceip+" "+sourceport+" "+destip+" "+destport+" "+protocol
+       rijdict.update({'sourceip': sourceip})
        rij.append(sourceip)
+       
+       rijdict.update({'sourceport': sourceport})
        rij.append(sourceport)
+       
+       rijdict.update({'destip': destip})
        rij.append(destip)
+       
+       rijdict.update({'destport': destport})
        rij.append(destport)
+       
+       rijdict.update({'protocol': protocol})
        rij.append(protocol)
        count = count + 1
 
       #only collect LAN/WAN statistics for optimized sessions
+      #note: these lan/wan statistics are on another line as the previous statistics (wan is first)
+      #lan line concludes all data -> save
+      
       if (parsed_arg.func == 'opt'):
        lan = []
        wan = []
        wan = re.findall(r'WAN: (\d*)KB', line)
        lan = re.findall(r'LAN: (\d*)KB', line)
       
+       
        if (wan != []): 
         #print wan[0]
         rij.append(wan[0])
+        rijdict.update({'wan': wan})
+       
        if (lan != []):
         #print lan[0]
         rij.append(lan[0])
-       
+        rijdict.update({'lan': lan})
+        #conclude collection (if opt)
+        pandas_rij.append(rijdict)
+      
+      else:      
+       #not opt command, no need to collect lan/wan statistics
+       #just conclude
+       #copy row to array
+       pandas_rij.append(rijdict)
+      
      if (parsed_arg.func == 'opt'): 
       # 7 parameters per row     
       multi = zip(rij[0::7],rij[1::7],rij[2::7],rij[3::7],rij[4::7],rij[5::7],rij[6::7])
      else:
       # 5 parameters per row
       multi = zip(rij[0::5],rij[1::5],rij[2::5],rij[3::5],rij[4::5])
+     
+     #convert to pandas dataframe
+     df = pd.DataFrame(pandas_rij)   
+     
      if (self.debug): log.debug(multi)
+     if (self.debug): log.debug(df)
      #print multi[100]
      
      
@@ -281,6 +311,8 @@ show set       : Show settings"""
      #Execute additional filtering on TCP if tcp parameter is given
      if (parsed_arg.tcp):
       count = 0
+      #just one simple line with pandas
+      df = df[df.destport == parsed_arg.tcp]
       for line in multi:
        if (line[3] == parsed_arg.tcp):
         #print line
@@ -288,105 +320,138 @@ show set       : Show settings"""
         count = count + 1
       multi = filtered  
     
-     
+     if (self.debug): log.debug(df)
      
 
      #AGGREGATE PER TCP
      #extract TCP ports out of array (TCP = index 3)
      aggregate = [x[3] for x in multi]
+     #and sorted immediatly in pandas
+     pandas_a = df.groupby('destport').count().sort('destip',ascending=False)
+     
      
      #if tcp is specified, aggregate on destination IP address (index = 2) by default
      #if client option is specified, aggregate by client ip address instead
-     #else nothing specified, TCP default
+     #else nothing specified, TCP default (keep the above)
      if (parsed_arg.tcp) and not(parsed_arg.clients):
       aggregate = [x[2] for x in multi]
+      pandas_a = df.groupby('destip').count().sort('destport',ascending=False)
       log.info('Destination IP  : #')
      elif (parsed_arg.tcp) and (parsed_arg.clients):
       aggregate = [x[0] for x in multi]
+      pandas_a = df.groupby('sourceip').count().sort('sourceport',ascending=False)
       log.info('Client IP       : #')
      else: log.info('TCP             : #')
      
-     
+     if (self.debug): log.debug(pandas_a)
      
      log.info('--------------------------------------')
 	
      #aggregate by nth element
+     #maximum = top parameter; 0 = print all, no top
      if (maximum == 0): 
       sumprot = Counter(aggregate)
       if (self.debug): print sumprot
       #print counter var (set to only take unique values)
-      for element in set(sumprot.elements()):
-       log.info('{0: <16}: {1: <16}'.format(element,sumprot[element]))
-	 	 
+      #for element in set(sumprot.elements()):
+      # log.info('{0: <16}: {1: <16}'.format(element,sumprot[element]))
+      #pandas
+      
+      #note: instead of iterating row by row, we can also just print the darn thing with only one column and the column name changed
+      #formatting is done by pandas then  (maybe for future versions)
+      for index, row in pandas_a.iterrows():
+       #need to be carefull here about which column we select, can't select destip
+       #when aggregated on destip, this columnindex does not exist anymore
+       #protocol is always there
+       log.info('{0: <16}: {1: <16}'.format(index,row['protocol']))
+     
+	 
+	 #maximum is given (ie top 10)
      else:
       sumprot = Counter(aggregate).most_common(int(maximum))
       if (self.debug): print sumprot
-	  #sumprot is now not a counterobject anymore, but notmal list
-      for a,b in sumprot:
-       log.info('{0: <16}: {1: <16}'.format(a,b))
+	  #sumprot is now not a counterobject anymore, but normal list
+      #old style
+      #for a,b in sumprot:
+      # log.info('{0: <16}: {1: <16}'.format(a,b))
+      #pandas style
+      #pandas_a is already sorted
+      pandas_a = pandas_a.head(int(maximum))
+      
+      if (self.debug): log.debug(pandas_a)
+     
+      for index, row in pandas_a.iterrows():
+       #need to be carefull here about which column we select, can't select destip
+       #when aggregated on destip, this columnindex does not exist anymore
+       #protocol is always there
+       log.info('{0: <16}: {1: <16}'.format(index,row['protocol']))
     
      
-     log.info('Total Sessions: ' + str(count))
-     log.info("========== end of data ========= ")
-	 
-  elif l[0] == 'opt_ssl':
-    for host, conn, naam in zip(self.hosts, self.connections, self.hostnames):
-     log.info(naam + ' : Optimized SSL (TCP443) Connections Overview')
-     log.info('------------------------------------------------------------')
-     channel = conn.invoke_shell()
-     channel.settimeout(10800)
-     channel.send('term length 0\n')
-     channel.send('show connections optimized\n')
-     buff = ''
-     while not buff.endswith('> '):
-      resp = channel.recv(9999)
-      buff += resp
-      if (self.debug): log.debug(buff)
-     opt_conn = buff
-
-     count = 0
-     rij = []
-     for line in StringIO.StringIO(opt_conn):
-      line = line.replace("\n","")
-      if line.startswith('O'):
-       parts = line.split()
-       source = parts[1]
-       (sourceip,sourceport) = source.split(':')
-       destination = parts[2]
-       (destip,destport) = destination.split(':')
-       protocol = parts[3]
-       rate = parts[4]
-       datum = parts[5]
-       tijd = parts[6]
-       #print sourceip+" "+sourceport+" "+destip+" "+destport+" "+protocol
-       if (destport == '443'):
-        rij.append(sourceip)
-        rij.append(sourceport)
-        rij.append(destip)
-        rij.append(destport)
-        rij.append(protocol)
-        count = count + 1
-
-     multi = zip(rij[0::5],rij[1::5],rij[2::5],rij[3::5],rij[4::5])
-     #print multi
-     #print multi[100]
-
-     #extract nth element in array
-     protocols = [x[2] for x in multi]
-	
-     log.info('HOST           : #')
-     log.info('---------------------------')
-	
-     #aggregate by nth element
-     sumprot = Counter(protocols)
-     #sumprot = counter object
-     #print counter var (set to only take unique values)
-     for element in set(sumprot.elements()):
-      log.info('{0: <15}: {1: <6}'.format(element,sumprot[element]))
-	 	 
-     log.info('Total SSL Optimized Sessions: ' + str(count))
+     log.info('Total Sessions: ' + str(pandas_a['protocol'].sum()))
      log.info("========== end of data ========= ")
 
+  # --
+  # -- removed "show opt_ssl" command, use "show opt tcp 443" instead which is just the same	 
+  # --
+  #elif l[0] == 'opt_ssl':
+  #  for host, conn, naam in zip(self.hosts, self.connections, self.hostnames):
+  #   log.info(naam + ' : Optimized SSL (TCP443) Connections Overview')
+  #   log.info('------------------------------------------------------------')
+  #   channel = conn.invoke_shell()
+  #   channel.settimeout(10800)
+  #   channel.send('term length 0\n')
+  #   channel.send('show connections optimized\n')
+  #   buff = ''
+  #   while not buff.endswith('> '):
+  #    resp = channel.recv(9999)
+  #    buff += resp
+  #    if (self.debug): log.debug(buff)
+  #   opt_conn = buff
+  #
+  #  count = 0
+  #   rij = []
+  #   for line in StringIO.StringIO(opt_conn):
+  #    line = line.replace("\n","")
+  #    if line.startswith('O'):
+  #     parts = line.split()
+  #     source = parts[1]
+  #     (sourceip,sourceport) = source.split(':')
+  #     destination = parts[2]
+  #     (destip,destport) = destination.split(':')
+  #     protocol = parts[3]
+  #     rate = parts[4]
+  #     datum = parts[5]
+  #     tijd = parts[6]
+  #     #print sourceip+" "+sourceport+" "+destip+" "+destport+" "+protocol
+  #     if (destport == '443'):
+  #      rij.append(sourceip)
+  #      rij.append(sourceport)
+  #      rij.append(destip)
+  #      rij.append(destport)
+  #      rij.append(protocol)
+  #      count = count + 1
+  #
+  #   multi = zip(rij[0::5],rij[1::5],rij[2::5],rij[3::5],rij[4::5])
+  #   #print multi
+  #   #print multi[100]
+  #
+  #   #extract nth element in array
+  #   protocols = [x[2] for x in multi]
+  #	
+  #   log.info('HOST           : #')
+  #   log.info('---------------------------')
+  #	
+  #   #aggregate by nth element
+  #   sumprot = Counter(protocols)
+  #   #sumprot = counter object
+  #   #print counter var (set to only take unique values)
+  #   for element in set(sumprot.elements()):
+  #    log.info('{0: <15}: {1: <6}'.format(element,sumprot[element]))
+  #	 	 
+  #   log.info('Total SSL Optimized Sessions: ' + str(count))
+  #   log.info("========== end of data ========= ")
+
+  else: log.info('Unknown argument. Use help show to see syntax.')
 
 
 
